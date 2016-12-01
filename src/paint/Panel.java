09887b2fb00d0ca24e2f06e8914451b4eed1e498
student <student@240-02.INFORMATYKA.local>
2016-12-01 10:19:50 +0100

    lab3_zad1_ex

--- src/paint/Panel.java
+++ src/paint/Panel.java
@@ -5,25 +5,30 @@ import java.awt.event.*;
 import javax.swing.*;
 
 public class Panel extends JPanel implements MouseListener{
+    private int x=-1;
+    private int y=-1;
 
     public Panel() {
         addMouseListener(this);
     }
     
-        public void mouseExited(MouseEvent e){
-            JOptionPane.showMessageDialog(null, e.getPoint().toString());
-        }
-        public void mouseEntered(MouseEvent e){
-            JOptionPane.showMessageDialog(null, e.getPoint().toString());
-        }
-        public void mousePressed(MouseEvent e){
-            JOptionPane.showMessageDialog(null, e.getPoint().toString());
-        }
-        public void mouseReleased(MouseEvent e){
-            JOptionPane.showMessageDialog(null, e.getPoint().toString());
-        }
+    public void mouseExited(MouseEvent e){
+        JOptionPane.showMessageDialog(null, e.getPoint().toString());
+    }
 
-        public void mouseClicked(MouseEvent e) {
-            JOptionPane.showMessageDialog(null, e.getPoint().toString());
-        }
+    public void mouseEntered(MouseEvent e){
+        JOptionPane.showMessageDialog(null, e.getPoint().toString());
+    }
+
+    public void mousePressed(MouseEvent e){
+        JOptionPane.showMessageDialog(null, e.getPoint().toString());
+    }
+
+    public void mouseReleased(MouseEvent e){
+        JOptionPane.showMessageDialog(null, e.getPoint().toString());
+    }
+
+    public void mouseClicked(MouseEvent e) {
+        JOptionPane.showMessageDialog(null, e.getPoint().toString());
+    }
 }
